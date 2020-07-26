95d3dd7815d074d6d588e3d905aa8cf6458409b5
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-07-26 20:25:55 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,7 @@ app.get("/nxfw/latest", async function (_, res) {
     "https://darthsternie.net/switch-firmwares/"
   );
 
-  const result = reg.exec(response.data);
-
-  const json = {
-    url: result[2],
-    version: result[1],
-  };
-
-  res.json(json);
+  res.json(getJson(response.data)[0]);
 });
 
 app.get("/nxfw/list", async function (_, res) {
@@ -25,16 +18,7 @@ app.get("/nxfw/list", async function (_, res) {
     "https://darthsternie.net/switch-firmwares/"
   );
 
-  const arr = [];
-  let result;
-  while ((result = reg.exec(response.data)) !== null) {
-    arr.push({
-      url: result[2],
-      version: result[1],
-    });
-  }
-
-  res.json(arr);
+  res.json(getJson(response.data));
 });
 
 app.get("/nxfw/:ver", async function (req, res) {
@@ -42,22 +26,24 @@ app.get("/nxfw/:ver", async function (req, res) {
     "https://darthsternie.net/switch-firmwares/"
   );
 
-  const vers = [];
-  const urls = [];
-  let result;
-  while ((result = reg.exec(response.data)) !== null) {
-    vers.push(result[1]);
-    urls.push(result[2]);
-  }
-
-  const idx = vers.indexOf(req.params.ver);
-  if (idx != -1) {
-    res.json({ url: urls[idx] });
-  } else {
-    res.json({ error_code: 404 });
-  }
+  let json = getJson(response.data).filter(
+    (obj) => obj.version == req.params.ver
+  );
+  res.json(json.length ? json : { error_code: 404 });
 });
 
 app.listen(80, function () {
   console.log("App listening on port 80!");
 });
+
+function getJson(data) {
+  const arr = [];
+  let result;
+  while ((result = reg.exec(data)) !== null) {
+    arr.push({
+      url: result[2],
+      version: result[1],
+    });
+  }
+  return arr;
+}
